8816ec0b9efd58e270284cd19116e38c81a6e79d
Gaius Fonseca <gaiusfonseca@gmail.com>
2023-03-07 00:53:40 -0300

    creating test templates

---
+++ src/test/java/br/gaius/webscrapper/TextHandlerTest.java
@@ -0,0 +1,32 @@
+package br.gaius.webscrapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.Test;
+
+class TextHandlerTest {
+
+	@Test
+	void should_MatchPattern_When_FileIsCreated() {
+		fail("need to develop test");
+	}
+	
+	@Test
+	void should_ReturnTrue_When_FileExists() {
+		fail("need to develop test");
+	}
+	
+	@Test
+	void should_ThrowFileNotFoundException_When_CanNotBeCreated() {
+		fail("need to develop test");
+	}
+	
+	@Test
+	void should_ThrowSecurityException_When_DoesNotHaveAccess() {
+		fail("need to develop test");
+	}
+	
+
+}
